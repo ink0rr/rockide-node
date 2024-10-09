b97b7e797c88ada93a8273c6a6b5bec407afd746
ink0rr <tomflynnbeltsazar@gmail.com>
2024-10-10 01:10:52 +0800

    Remove unused var

--- src/semantics.ts
+++ src/semantics.ts
@@ -6,37 +6,6 @@ export type SemanticToken = {
   modifiers?: string[];
 };
 
-export const molangSemantics: SemanticToken[] = [
-  {
-    pattern: /\b(q|v|t|c|query|variable|temp|context|math|array|geometry|material|texture)(?=\.)/gi,
-    type: "class",
-  },
-  {
-    pattern: /(?<=\b(q|v|t|c|query|variable|temp|context|math|array|geometry|material|texture))\.[\w.]+\b/gi,
-    type: "function",
-  },
-  {
-    pattern: /\bthis\b/gi,
-    type: "macro",
-  },
-  {
-    pattern: /(?<!\.)\b\d+(\.\d+)?(?!\.)f?\b/g,
-    type: "number",
-  },
-  {
-    pattern: /[=<>!&|,;?]+/g,
-    type: "operator",
-  },
-  {
-    pattern: /[\(\)\{\}\[\]]/g,
-    type: "enumMember",
-  },
-  {
-    pattern: /'[^']*'/g,
-    type: "regexp",
-  },
-];
-
 export const legend = new vscode.SemanticTokensLegend([
   "class",
   "enumMember",
